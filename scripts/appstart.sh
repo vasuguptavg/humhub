eda837476b57e26beac267c580e3ecea9379ac2d
Vasu Gupta <36095339+vasuguptavg@users.noreply.github.com>
2020-04-10 13:08:52 +0530

    Delete appstart.sh

--- scripts/appstart.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-sudo sed -i 's|/var/www/html|/var/www/html/humhub|g' /etc/apache2/sites-available/000-default.conf
-sudo sed -i '10i\        \<Directory /var/www/html>' /etc/apache2/sites-available/000-default.conf
-sudo sed -i '11i\           \AllowOverride All'      /etc/apache2/sites-available/000-default.conf
-sudo sed -i '12i\        \</Directory>'              /etc/apache2/sites-available/000-default.conf
-sudo systemctl restart apache2
